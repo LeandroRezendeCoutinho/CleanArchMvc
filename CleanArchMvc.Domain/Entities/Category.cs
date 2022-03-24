47603c145e27d5aac6460a18eaf6b5bfaff4c92d
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2022-03-24 08:36:48 -0300

    feat: add category and product entities

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,9 @@
+namespace CleanArchMvc.Domain.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
