27211890511f58d98590c6b9095ff4a518764db3
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2022-03-24 09:03:10 -0300

    feat: add category domain validation

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -8,12 +8,23 @@ namespace CleanArchMvc.Domain.Entities
 
         public Category(string name)
         {
-            Name = name;
+            ValidateDomain(name);
         }
 
         public Category(int id, string name)
         {
-            Id = id;
+            DomainExceptionValidation.When(id < 0, "Invalid ID");
+            ValidateDomain(name);
+        }
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+                "Invalid Name. Name is required");
+
+            DomainExceptionValidation.When(name.Length < 3,
+                "Invalid Name. Name must have at least 3 characters");
+
             Name = name;
         }
     }
