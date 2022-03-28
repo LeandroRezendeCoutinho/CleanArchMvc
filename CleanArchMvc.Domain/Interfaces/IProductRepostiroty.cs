87961dfc535d5252798dbdbbcde7d653ca929a6e
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2022-03-28 08:22:46 -0300

    feat: add domain entities interfaces

---
+++ CleanArchMvc.Domain/Interfaces/IProductRepostiroty.cs
@@ -0,0 +1,14 @@
+using CleanArchMvc.Domain.Entities;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    public interface IProductRepostiroty
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetByIdAsync(int? id);
+        Task<Product> GetProductCategoryAsync(int? id);
+        Task<Product> CreateAsync(Product category);
+        Task<Product> UpdateAsync(Product category);
+        Task<Product> RemoveAsync(Product category);
+    }
+}
