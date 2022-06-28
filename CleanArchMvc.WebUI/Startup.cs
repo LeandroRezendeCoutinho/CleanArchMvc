4449eadabc430f3cf8488bd883281a36bde27b85
Leandro Rezende <leandro.rc.contato@gmail.com>
2022-06-28 09:58:53 -0300

    fix: clean up

--- CleanArchMvc.WebUI/Startup.cs
+++ CleanArchMvc.WebUI/Startup.cs
@@ -1,10 +1,5 @@
 ﻿using CleanArchMvc.Domain.Account;
 using CleanArchMvc.Infra.IoC;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace CleanArchMvc.WebUI
 {
